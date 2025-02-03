0751406ad1f74c8dbdeb0dffc2bc4b1e72a72da8
mrdancingcrab <jaederberg@gmail.com>
2025-02-03 17:17:06 +0100

    hello

---
+++ Labb2DotNetMongoAPI/Data/MongoDbContext.cs
@@ -0,0 +1,80 @@
+﻿using Labb2DotNetMongoAPI.Models;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace Labb2DotNetMongoAPI.Data
+{
+    public class MongoDbContext
+    {
+        private IMongoDatabase db;
+
+        public MongoDbContext(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+
+        //Add player
+        public async Task<List<Players>> AddPlayer(string table, Players player)
+        {
+            var collection = db.GetCollection<Players>(table);
+            await collection.InsertOneAsync(player);
+            return collection.AsQueryable().ToList();
+        }
+
+
+        //Get all players
+        public async Task<List<Players>> GetAllPlayers(string table)
+        {
+            var collection = db.GetCollection<Players>(table);
+            var players = await collection.AsQueryable().ToListAsync();
+            return players;
+
+        }
+
+
+        //Get player by id
+        public async Task<Players> GetPlayerById(string table, string id)
+        {
+            var collection = db.GetCollection<Players>(table);
+            var player = await collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+            return player;
+
+        }
+
+
+        //Update player by id
+        public async Task<string> UpdatePlayerById(string table, string id, Players updatedPlayer)
+        {
+            var collection = db.GetCollection<Players>(table);
+
+            var existingPlayer = await collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+            if (existingPlayer == null)
+            {
+                return "Could not find player";
+            }
+            else
+            {
+                var update = Builders<Players>.Update
+                    .Set(x => x.FirstName, updatedPlayer.FirstName)
+                    .Set(x => x.LastName, updatedPlayer.LastName)
+                    .Set(x => x.Country, updatedPlayer.Country)
+                    .Set(x => x.Position, updatedPlayer.Position)
+                    .Set(x => x.Club, updatedPlayer.Club);
+
+                await collection.UpdateOneAsync(x => x.Id == id, update);
+                return "Player updated";
+
+            }
+        }
+
+        //Delete player
+        public async Task<string> DeletePlayer(string table, string id)
+        {
+            var collection = db.GetCollection<Players>(table);
+            var player = await collection.DeleteOneAsync(x => x.Id == id);
+            return "Deleted Player";
+
+        }
+    }
+}
---
+++ Labb2DotNetMongoAPI/Program.cs
@@ -0,0 +1,84 @@
+using Labb2DotNetMongoAPI.Models;
+using Labb2DotNetMongoAPI.Data;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+namespace Labb2DotNetMongoAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            MongoDbContext db = new MongoDbContext("FootballPlayers");
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+           // app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            //CREATE POKEMON
+            app.MapPost("/player", async (Players newPlayer) =>
+            {
+                var player = await db.AddPlayer("Players", newPlayer);
+                return Results.Ok(player);
+            });
+
+            //GET ALL
+            app.MapGet("/players", async () =>
+            {
+                var pokemons = await db.GetAllPlayers("Players");
+                return Results.Ok(pokemons);
+            });
+
+            //GET POKEMON BY ID
+            app.MapGet("/player/{id}", async (string id) =>
+            {
+                var pokemon = await db.GetPlayerById("Players", id);
+                return Results.Ok(pokemon);
+            });
+
+            //UPDATE POKEMON
+            app.MapPut("/player/{id}", async (string id, Players updatedPlayer) =>
+            {
+                var player = await db.UpdatePlayerById("Players", id, updatedPlayer);
+
+                if (player == null)
+                {
+                    return Results.NotFound("Could not find player");
+                }
+                else
+                {
+                    return Results.Ok(player);
+                }
+            });
+
+            //DELETE POKEMON
+            app.MapDelete("/player/{id}", async (string id) =>
+            {
+                var message = await db.DeletePlayer("Players", id);
+                return "Player deleted";
+            });
+
+            app.Run();
+        }
+    }
+}
